01a51566d984d0651118e8b1d5e6cbe14d5e55c3
avivt <aviv.tamar.mail@gmail.com>
2021-05-18 10:46:04 +0300

    added logging

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ from a2c_ppo_acktr.envs import make_vec_envs
 from a2c_ppo_acktr.model import Policy
 from a2c_ppo_acktr.storage import RolloutStorage
 from evaluation import evaluate
+from a2c_ppo_acktr.utils import save_obj, load_obj
 
 EVAL_ENVS = {'three_arms': 'h_bandit-randchoose-v0',
              'five_arms': 'h_bandit-randchoose-v2',
@@ -36,10 +37,30 @@ def main():
         torch.backends.cudnn.benchmark = False
         torch.backends.cudnn.deterministic = True
 
-    log_dir = os.path.expanduser(args.log_dir)
-    eval_log_dir = log_dir + "_eval"
-    utils.cleanup_log_dir(log_dir)
-    utils.cleanup_log_dir(eval_log_dir)
+    logdir = args.env_name + '_' + args.algo + '_num_arms_' + str(args.num_processes) + '_' + time.strftime("%d-%m-%Y_%H-%M-%S")
+    if args.use_privacy:
+        logdir = logdir + '_privacy'
+    elif args.use_noisygrad:
+        logdir = logdir + '_noisygrad'
+    elif args.use_pcgrad:
+        logdir = logdir + '_pcgrad'
+    logdir = os.path.join('runs', logdir)
+    logdir = os.path.join(os.path.expanduser(args.log_dir), logdir)
+    utils.cleanup_log_dir(logdir)
+
+    # Ugly but simple logging
+    log_dict = {
+        'task_steps': args.task_steps,
+        'grad_noise_ratio': args.grad_noise_ratio,
+        'max_task_grad_norm': args.max_task_grad_norm,
+        'use_noisygrad': args.use_noisygrad,
+        'use_pcgrad': args.use_noisygrad,
+        'use_privacy': args.use_privacy,
+        'seed': args.seed,
+        'cmd': ' '.join(sys.argv[1:])
+    }
+    for eval_disp_name, eval_env_name in EVAL_ENVS.items():
+        log_dict[eval_disp_name] = []
 
     summary_writer = SummaryWriter()
     summary_writer.add_hparams({'task_steps': args.task_steps,
@@ -95,9 +116,6 @@ def main():
             use_privacy=args.use_privacy,
             max_task_grad_norm=args.max_task_grad_norm,
             grad_noise_ratio=args.grad_noise_ratio)
-    elif args.algo == 'acktr':
-        agent = algo.A2C_ACKTR(
-            actor_critic, args.value_loss_coef, args.entropy_coef, acktr=True)
 
     rollouts = RolloutStorage(args.num_steps, args.num_processes,
                               envs.observation_space.shape, envs.action_space,
@@ -195,9 +213,14 @@ def main():
             for eval_disp_name, eval_env_name in EVAL_ENVS.items():
                 print(eval_disp_name)
                 eval_r[eval_disp_name] = evaluate(actor_critic, obs_rms, eval_env_name, args.seed,
-                                                  args.num_processes, eval_log_dir, device, steps=args.task_steps)
+                                                  args.num_processes, logdir, device, steps=args.task_steps)
                 summary_writer.add_scalar(f'eval/{eval_disp_name}', eval_r[eval_disp_name], (j+1) * args.num_processes * args.num_steps)
+                log_dict[eval_disp_name].append([(j+1) * args.num_processes * args.num_steps, eval_r[eval_disp_name]])
             summary_writer.add_scalars('eval_combined', eval_r, (j+1) * args.num_processes * args.num_steps)
             actor_critic.train()
+
+    save_obj(log_dict, os.path.join(logdir, 'log_dict.pkl'))
+
+
 if __name__ == "__main__":
     main()
