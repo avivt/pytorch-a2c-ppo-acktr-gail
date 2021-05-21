cf705409adb3f62c6da59e22b35533e8b2ab073b
avivt <aviv.tamar.mail@gmail.com>
2021-05-21 23:27:55 +0300

    script

---
+++ a2c_ppo_acktr/scripts/script_base_rec4.sh
@@ -0,0 +1,21 @@
+python3 main.py --env-name "h_bandit-randchoose-v6" --algo ppo --log-interval 25 --num-steps 100 --num-processes 5 --lr 3e-4 --entropy-coef 0.05 --value-loss-coef 0.5 --ppo-epoch 1 --num-mini-batch 5 --gamma 0.9 --gae-lambda 0.95 --num-env-steps 3000000 --eval-interval 100 --log-dir ./ppo_log --task_steps=20 --recurrent-policy  --seed 1 &
+
+sleep 3
+
+python3 main.py --env-name "h_bandit-randchoose-v6" --algo ppo --log-interval 25 --num-steps 100 --num-processes 5 --lr 3e-4 --entropy-coef 0.05 --value-loss-coef 0.5 --ppo-epoch 1 --num-mini-batch 5 --gamma 0.9 --gae-lambda 0.95 --num-env-steps 3000000 --eval-interval 100 --log-dir ./ppo_log --task_steps=20 --recurrent-policy  --seed 2 &
+
+sleep 3
+
+python3 main.py --env-name "h_bandit-randchoose-v6" --algo ppo --log-interval 25 --num-steps 100 --num-processes 5 --lr 3e-4 --entropy-coef 0.05 --value-loss-coef 0.5 --ppo-epoch 1 --num-mini-batch 5 --gamma 0.9 --gae-lambda 0.95 --num-env-steps 3000000 --eval-interval 100 --log-dir ./ppo_log --task_steps=20 --recurrent-policy  --seed 3 &
+
+sleep 3
+
+python3 main.py --env-name "h_bandit-randchoose-v6" --algo ppo --log-interval 25 --num-steps 100 --num-processes 5 --lr 3e-4 --entropy-coef 0.05 --value-loss-coef 0.5 --ppo-epoch 1 --num-mini-batch 5 --gamma 0.9 --gae-lambda 0.95 --num-env-steps 3000000 --eval-interval 100 --log-dir ./ppo_log --task_steps=20 --recurrent-policy  --seed 4 &
+
+sleep 3
+
+python3 main.py --env-name "h_bandit-randchoose-v6" --algo ppo --log-interval 25 --num-steps 100 --num-processes 5 --lr 3e-4 --entropy-coef 0.05 --value-loss-coef 0.5 --ppo-epoch 1 --num-mini-batch 5 --gamma 0.9 --gae-lambda 0.95 --num-env-steps 3000000 --eval-interval 100 --log-dir ./ppo_log --task_steps=20 --recurrent-policy  --seed 5 &
+
+wait
+
+echo "check baseline on 5 arms recurrent training, without free exploration on the randomly generated domains. See if this works worse than with free exploration. If it does, interesting if testgrad helps without free exploration"
