321c3991f01d4c9c0dedcda2a383fd8daf0844ba
avivt <aviv.tamar.mail@gmail.com>
2021-05-23 15:00:09 +0300

    bug in evaluation

--- evaluation.py
+++ evaluation.py
@@ -25,7 +25,8 @@ def evaluate(actor_critic, obs_rms, env_name, seed, num_processes, eval_log_dir,
             num_processes, actor_critic.recurrent_hidden_state_size, device=device)
         eval_masks = torch.zeros(num_processes, 1, device=device)
 
-        while len(eval_episode_rewards) < 1:
+        # while len(eval_episode_rewards) < 1:
+        for t in range(kwargs["steps"]):
             with torch.no_grad():
                 _, action, _, eval_recurrent_hidden_states = actor_critic.act(
                     obs,
@@ -50,5 +51,4 @@ def evaluate(actor_critic, obs_rms, env_name, seed, num_processes, eval_log_dir,
 
     # print(" Evaluation using {} episodes: mean reward {:.5f}\n".format(
     #     len(eval_episode_rewards), np.mean(eval_episode_rewards)))
-
     return np.mean(eval_episode_rewards)
