e30821f22bf73d7dfdc96389a2157c48f7f431a6
avivt <aviv.tamar.mail@gmail.com>
2021-05-24 11:35:18 +0300

    fix bug in free_exploration. add weight decay

--- a2c_ppo_acktr/scripts/script_rec28.sh
+++ a2c_ppo_acktr/scripts/script_rec28.sh
@@ -1,24 +1,24 @@
-python3 main.py --env-name "h_bandit-randchoose-v5" --algo ppo --log-interval 25 --num-steps 100 --num-processes 10 --lr 3e-4 --entropy-coef 0.05 --value-loss-coef 0.5 --ppo-epoch 1 --num-mini-batch 10 --gamma 0.9 --gae-lambda 0.95 --num-env-steps 3000000 --eval-interval 100 --log-dir ./ppo_log --task_steps=20  --obs_recurrent --use_testgrad --max_task_grad_norm 5.0 --grad_noise_ratio 0.0 --free_exploration 6 --weight_decay 0.1 --seed 1 &
+python3 main.py --env-name "h_bandit-randchoose-v5" --algo ppo --log-interval 25 --num-steps 100 --num-processes 10 --lr 3e-4 --entropy-coef 0.05 --value-loss-coef 0.5 --ppo-epoch 1 --num-mini-batch 10 --gamma 0.9 --gae-lambda 0.95 --num-env-steps 3000000 --eval-interval 100 --log-dir ./ppo_log --task_steps=20  --obs_recurrent --use_testgrad --max_task_grad_norm 5.0 --grad_noise_ratio 0.0 --free_exploration 6 --weight_decay 0.0001 --seed 1 &
 
 sleep 3
 
-python3 main.py --env-name "h_bandit-randchoose-v5" --algo ppo --log-interval 25 --num-steps 100 --num-processes 10 --lr 3e-4 --entropy-coef 0.05 --value-loss-coef 0.5 --ppo-epoch 1 --num-mini-batch 10 --gamma 0.9 --gae-lambda 0.95 --num-env-steps 3000000 --eval-interval 100 --log-dir ./ppo_log --task_steps=20  --obs_recurrent --use_testgrad --max_task_grad_norm 5.0 --grad_noise_ratio 0.0 --free_exploration 6 --weight_decay 0.1 --seed 2 &
+python3 main.py --env-name "h_bandit-randchoose-v5" --algo ppo --log-interval 25 --num-steps 100 --num-processes 10 --lr 3e-4 --entropy-coef 0.05 --value-loss-coef 0.5 --ppo-epoch 1 --num-mini-batch 10 --gamma 0.9 --gae-lambda 0.95 --num-env-steps 3000000 --eval-interval 100 --log-dir ./ppo_log --task_steps=20  --obs_recurrent --use_testgrad --max_task_grad_norm 5.0 --grad_noise_ratio 0.0 --free_exploration 6 --weight_decay 0.0001 --seed 2 &
 
 sleep 3
 
-python3 main.py --env-name "h_bandit-randchoose-v5" --algo ppo --log-interval 25 --num-steps 100 --num-processes 10 --lr 3e-4 --entropy-coef 0.05 --value-loss-coef 0.5 --ppo-epoch 1 --num-mini-batch 10 --gamma 0.9 --gae-lambda 0.95 --num-env-steps 3000000 --eval-interval 100 --log-dir ./ppo_log --task_steps=20  --obs_recurrent --use_testgrad --max_task_grad_norm 5.0 --grad_noise_ratio 0.0 --free_exploration 6 --weight_decay 0.1 --seed 3 &
+python3 main.py --env-name "h_bandit-randchoose-v5" --algo ppo --log-interval 25 --num-steps 100 --num-processes 10 --lr 3e-4 --entropy-coef 0.05 --value-loss-coef 0.5 --ppo-epoch 1 --num-mini-batch 10 --gamma 0.9 --gae-lambda 0.95 --num-env-steps 3000000 --eval-interval 100 --log-dir ./ppo_log --task_steps=20  --obs_recurrent --use_testgrad --max_task_grad_norm 5.0 --grad_noise_ratio 0.0 --free_exploration 6 --weight_decay 0.0001 --seed 3 &
 
 sleep 3
 
-python3 main.py --env-name "h_bandit-randchoose-v5" --algo ppo --log-interval 25 --num-steps 100 --num-processes 10 --lr 3e-4 --entropy-coef 0.05 --value-loss-coef 0.5 --ppo-epoch 1 --num-mini-batch 10 --gamma 0.9 --gae-lambda 0.95 --num-env-steps 3000000 --eval-interval 100 --log-dir ./ppo_log --task_steps=20  --obs_recurrent --use_testgrad --max_task_grad_norm 5.0 --grad_noise_ratio 0.0 --free_exploration 6 --weight_decay 0.01 --seed 4 &
+python3 main.py --env-name "h_bandit-randchoose-v5" --algo ppo --log-interval 25 --num-steps 100 --num-processes 10 --lr 3e-4 --entropy-coef 0.05 --value-loss-coef 0.5 --ppo-epoch 1 --num-mini-batch 10 --gamma 0.9 --gae-lambda 0.95 --num-env-steps 3000000 --eval-interval 100 --log-dir ./ppo_log --task_steps=20  --obs_recurrent --use_testgrad --max_task_grad_norm 5.0 --grad_noise_ratio 0.0 --free_exploration 6 --weight_decay 0.0005 --seed 4 &
 
 sleep 3
 
-python3 main.py --env-name "h_bandit-randchoose-v5" --algo ppo --log-interval 25 --num-steps 100 --num-processes 10 --lr 3e-4 --entropy-coef 0.05 --value-loss-coef 0.5 --ppo-epoch 1 --num-mini-batch 10 --gamma 0.9 --gae-lambda 0.95 --num-env-steps 3000000 --eval-interval 100 --log-dir ./ppo_log --task_steps=20  --obs_recurrent --use_testgrad --max_task_grad_norm 5.0 --grad_noise_ratio 0.0 --free_exploration 6 --weight_decay 0.01 --seed 5 &
+python3 main.py --env-name "h_bandit-randchoose-v5" --algo ppo --log-interval 25 --num-steps 100 --num-processes 10 --lr 3e-4 --entropy-coef 0.05 --value-loss-coef 0.5 --ppo-epoch 1 --num-mini-batch 10 --gamma 0.9 --gae-lambda 0.95 --num-env-steps 3000000 --eval-interval 100 --log-dir ./ppo_log --task_steps=20  --obs_recurrent --use_testgrad --max_task_grad_norm 5.0 --grad_noise_ratio 0.0 --free_exploration 6 --weight_decay 0.0005 --seed 5 &
 
 sleep 3
 
-python3 main.py --env-name "h_bandit-randchoose-v5" --algo ppo --log-interval 25 --num-steps 100 --num-processes 10 --lr 3e-4 --entropy-coef 0.05 --value-loss-coef 0.5 --ppo-epoch 1 --num-mini-batch 10 --gamma 0.9 --gae-lambda 0.95 --num-env-steps 3000000 --eval-interval 100 --log-dir ./ppo_log --task_steps=20  --obs_recurrent --use_testgrad --max_task_grad_norm 5.0 --grad_noise_ratio 0.0 --free_exploration 6 --weight_decay 0.01 --seed 6 &
+python3 main.py --env-name "h_bandit-randchoose-v5" --algo ppo --log-interval 25 --num-steps 100 --num-processes 10 --lr 3e-4 --entropy-coef 0.05 --value-loss-coef 0.5 --ppo-epoch 1 --num-mini-batch 10 --gamma 0.9 --gae-lambda 0.95 --num-env-steps 3000000 --eval-interval 100 --log-dir ./ppo_log --task_steps=20  --obs_recurrent --use_testgrad --max_task_grad_norm 5.0 --grad_noise_ratio 0.0 --free_exploration 6 --weight_decay 0.0005 --seed 6 &
 
 sleep 3
 
