4a6b83a886bb771d4c26711255b638478bf8d3d7
avivt <aviv.tamar.mail@gmail.com>
2021-05-26 17:19:45 +0300

    more envs

--- gym_envs/__init__.py
+++ gym_envs/__init__.py
@@ -94,6 +94,16 @@ register(
 )
 
 register(
+    id='h_bandit-randchoose-v8',
+    entry_point='gym.envs.toy_text:HBanditEnv',
+    kwargs={'r_dist_name': 'train_25',
+            'steps': 20
+            },
+    max_episode_steps=20,
+    reward_threshold=0.7, # optimum = .8196
+)
+
+register(
     id='h_bandit-randchoose-v1',
     entry_point='gym.envs.toy_text:HBanditEnv',
     kwargs={'r_dist_name': 'test',
