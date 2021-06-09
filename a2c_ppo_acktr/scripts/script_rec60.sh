bf6333c404106663ca900917b324f85044074c2f
avivt <aviv.tamar.mail@gmail.com>
2021-06-09 08:07:13 +0300

    script

---
+++ a2c_ppo_acktr/scripts/script_rec60.sh
@@ -0,0 +1,43 @@
+python3 dual_rl.py --env-name "h_bandit-obs-randchoose-v8" --val_env_name "h_bandit-obs-randchoose-v9" --algo  ppo --log-interval 25 --num-steps 100 --num-processes 25 --lr 3e-4 --val_lr 1e-3 --entropy-coef 0.05 --value-loss-coef 0.5 --ppo-epoch 3 --num-mini-batch 25 --gamma 0.9 --gae-lambda 0.95 --num-env-steps 10000000 --eval-interval 5 --log-dir ./ppo_log --task_steps=6 --obs_recurrent --free_exploration 6 --hard_attn --seed  3  &
+sleep 3
+
+python3 dual_rl.py --env-name "h_bandit-obs-randchoose-v8" --val_env_name "h_bandit-obs-randchoose-v9" --algo  ppo --log-interval 25 --num-steps 100 --num-processes 25 --lr 3e-4 --val_lr 1e-3 --entropy-coef 0.05 --value-loss-coef 0.5 --ppo-epoch 3 --num-mini-batch 25 --gamma 0.9 --gae-lambda 0.95 --num-env-steps 10000000 --eval-interval 5 --log-dir ./ppo_log --task_steps=6 --obs_recurrent --free_exploration 6 --hard_attn --seed  4  &
+sleep 3
+
+python3 dual_rl.py --env-name "h_bandit-obs-randchoose-v8" --val_env_name "h_bandit-obs-randchoose-v9" --algo  ppo --log-interval 25 --num-steps 100 --num-processes 25 --lr 3e-4 --val_lr 1e-3 --entropy-coef 0.05 --value-loss-coef 0.5 --ppo-epoch 3 --num-mini-batch 25 --gamma 0.9 --gae-lambda 0.95 --num-env-steps 10000000 --eval-interval 5 --log-dir ./ppo_log --task_steps=6 --obs_recurrent --free_exploration 6 --hard_attn --seed  5  &
+sleep 3
+
+python3 dual_rl.py --env-name "h_bandit-obs-randchoose-v8" --val_env_name "h_bandit-obs-randchoose-v9" --algo  ppo --log-interval 25 --num-steps 100 --num-processes 25 --lr 1e-3 --val_lr 1e-3 --entropy-coef 0.05 --value-loss-coef 0.5 --ppo-epoch 3 --num-mini-batch 25 --gamma 0.9 --gae-lambda 0.95 --num-env-steps 10000000 --eval-interval 5 --log-dir ./ppo_log --task_steps=6 --obs_recurrent --free_exploration 6 --hard_attn --seed  6  &
+sleep 3
+
+python3 dual_rl.py --env-name "h_bandit-obs-randchoose-v8" --val_env_name "h_bandit-obs-randchoose-v9" --algo  ppo --log-interval 25 --num-steps 100 --num-processes 25 --lr 1e-3 --val_lr 1e-3 --entropy-coef 0.05 --value-loss-coef 0.5 --ppo-epoch 3 --num-mini-batch 25 --gamma 0.9 --gae-lambda 0.95 --num-env-steps 10000000 --eval-interval 5 --log-dir ./ppo_log --task_steps=6 --obs_recurrent --free_exploration 6 --hard_attn --seed  7  &
+sleep 3
+
+python3 dual_rl.py --env-name "h_bandit-obs-randchoose-v8" --val_env_name "h_bandit-obs-randchoose-v9" --algo  ppo --log-interval 25 --num-steps 100 --num-processes 25 --lr 1e-3 --val_lr 1e-3 --entropy-coef 0.05 --value-loss-coef 0.5 --ppo-epoch 3 --num-mini-batch 25 --gamma 0.9 --gae-lambda 0.95 --num-env-steps 10000000 --eval-interval 5 --log-dir ./ppo_log --task_steps=6 --obs_recurrent --free_exploration 6 --hard_attn --seed  8  &
+sleep 3
+
+python3 dual_rl.py --env-name "h_bandit-obs-randchoose-v8" --val_env_name "h_bandit-obs-randchoose-v9" --algo  ppo --log-interval 25 --num-steps 100 --num-processes 25 --lr 7e-4 --val_lr 1e-3 --entropy-coef 0.05 --value-loss-coef 0.5 --ppo-epoch 3 --num-mini-batch 25 --gamma 0.9 --gae-lambda 0.95 --num-env-steps 10000000 --eval-interval 5 --log-dir ./ppo_log --task_steps=6 --obs_recurrent --free_exploration 6 --hard_attn --seed  9  &
+sleep 3
+
+python3 dual_rl.py --env-name "h_bandit-obs-randchoose-v8" --val_env_name "h_bandit-obs-randchoose-v9" --algo  ppo --log-interval 25 --num-steps 100 --num-processes 25 --lr 7e-4 --val_lr 1e-3 --entropy-coef 0.05 --value-loss-coef 0.5 --ppo-epoch 3 --num-mini-batch 25 --gamma 0.9 --gae-lambda 0.95 --num-env-steps 10000000 --eval-interval 5 --log-dir ./ppo_log --task_steps=6 --obs_recurrent --free_exploration 6 --hard_attn --seed  10  &
+sleep 3
+
+python3 dual_rl.py --env-name "h_bandit-obs-randchoose-v8" --val_env_name "h_bandit-obs-randchoose-v9" --algo  ppo --log-interval 25 --num-steps 100 --num-processes 25 --lr 7e-4 --val_lr 1e-3 --entropy-coef 0.05 --value-loss-coef 0.5 --ppo-epoch 3 --num-mini-batch 25 --gamma 0.9 --gae-lambda 0.95 --num-env-steps 10000000 --eval-interval 5 --log-dir ./ppo_log --task_steps=6 --obs_recurrent --free_exploration 6 --hard_attn --seed  11  &
+sleep 3
+
+python3 dual_rl.py --env-name "h_bandit-obs-randchoose-v8" --val_env_name "h_bandit-obs-randchoose-v9" --algo  ppo --log-interval 25 --num-steps 100 --num-processes 25 --lr 3e-4 --val_lr 3e-4 --entropy-coef 0.05 --value-loss-coef 0.5 --ppo-epoch 3 --num-mini-batch 25 --gamma 0.9 --gae-lambda 0.95 --num-env-steps 10000000 --eval-interval 5 --log-dir ./ppo_log --task_steps=6 --obs_recurrent --free_exploration 6 --hard_attn --seed  12  &
+sleep 3
+
+python3 dual_rl.py --env-name "h_bandit-obs-randchoose-v8" --val_env_name "h_bandit-obs-randchoose-v9" --algo  ppo --log-interval 25 --num-steps 100 --num-processes 25 --lr 3e-4 --val_lr 3e-4 --entropy-coef 0.05 --value-loss-coef 0.5 --ppo-epoch 3 --num-mini-batch 25 --gamma 0.9 --gae-lambda 0.95 --num-env-steps 10000000 --eval-interval 5 --log-dir ./ppo_log --task_steps=6 --obs_recurrent --free_exploration 6 --hard_attn --seed  13  &
+sleep 3
+
+python3 dual_rl.py --env-name "h_bandit-obs-randchoose-v8" --val_env_name "h_bandit-obs-randchoose-v9" --algo  ppo --log-interval 25 --num-steps 100 --num-processes 25 --lr 1e-3 --val_lr 3e-4 --entropy-coef 0.05 --value-loss-coef 0.5 --ppo-epoch 3 --num-mini-batch 25 --gamma 0.9 --gae-lambda 0.95 --num-env-steps 10000000 --eval-interval 5 --log-dir ./ppo_log --task_steps=6 --obs_recurrent --free_exploration 6 --hard_attn --seed  14  &
+sleep 3
+
+python3 dual_rl.py --env-name "h_bandit-obs-randchoose-v8" --val_env_name "h_bandit-obs-randchoose-v9" --algo  ppo --log-interval 25 --num-steps 100 --num-processes 25 --lr 1e-3 --val_lr 3e-4 --entropy-coef 0.05 --value-loss-coef 0.5 --ppo-epoch 3 --num-mini-batch 25 --gamma 0.9 --gae-lambda 0.95 --num-env-steps 10000000 --eval-interval 5 --log-dir ./ppo_log --task_steps=6 --obs_recurrent --free_exploration 6 --hard_attn --seed  15  &
+sleep 3
+
+wait
+
+echo "obs recurrent 25 arms with 25 validation arms, free exploration, dual_rl. Checking different hyperparamaters"
+
